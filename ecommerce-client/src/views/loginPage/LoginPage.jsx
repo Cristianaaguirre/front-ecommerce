496186d28cf4684aef1478fd7c6807e607471a63
Cristian Aguirre <cristianaaguirre0@gmail.com>
2022-06-15 11:49:46 -0300

    commit

--- ecommerce-client/src/views/loginPage/LoginPage.jsx
+++ ecommerce-client/src/views/loginPage/LoginPage.jsx
@@ -1,36 +1,71 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom'
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import style from './LoginPageStyle.module.css'
 
-import logo from '../../assets/logo.png'
+import { postLoginDataActionSaga } from '../../redux/actions/postLoginDataActions';
+import CustomSpinner from '../../components/Spinner/Spinner';
 
 const LoginPage = () => {
+
+    const dispatch = useDispatch()
+    
+    const visible = useSelector(store => store.setLoginFormVisibleReducer) // Boolean data from saga submit
+
+    // Manage login form
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required("Email required"),
+            password: Yup.string().required("Password required"),
+        }),
+        onSubmit: (values) => {
+            dispatch(postLoginDataActionSaga({
+                email: values.email,
+                password: values.password
+            }))
+        },
+    });
+
+    // Render page
     return (
-        <main className={style.contentMain}>
-            <article className={style.contentForm}>
-                <img className={style.logo} src={logo }alt="logo"/>
-                <form className={style.formikForm}>
-                    <div className={style.contentInput}>
-                        <label className={style.customLabel}>Your Email:</label>
-                        <input
-                            className={style.customInput} 
-                            type="email"
-                            placeholder="Enter your email"
-                        />
-                    </div>
-                    <div className={style.contentInput}>
-                        <label className={style.customLabel}>Password: </label>
-                        <input 
-                            className={style.customInput}
-                            type="password"
-                            placeholder="password"
-                        />
-                    </div>
-                    <input className={style.inputButton} type="submit" value="Login"/>
-                </form>
-                {/* you are a member? log in */}
-                <p className={style.textRegister}>
-                    <i>You are not a member? <a href="/"> Register</a></i>
-                </p>
+        <main className={style.container}>
+            <article className={style.wrapper}>
+                <h1 className={style.title}>SIGN IN</h1>
+                {
+                    visible ? 
+                        <form className={style.form} onSubmit={formik.handleSubmit}>
+                            <input
+                                placeholder="email"
+                                id="email"
+                                name="email"
+                                type="email"
+                                onChange={formik.handleChange}
+                                className={style.input} />
+                                {formik.errors.email ?
+                                        <div className={style.formikError}>
+                                            {" "}{formik.errors.email}{" "}
+                                        </div>
+                                    : null}
+                            <input
+                                placeholder="password"
+                                id="password"
+                                name="password"
+                                type="password"
+                                onChange={formik.handleChange} 
+                                className={style.input} />
+                            <input
+                                type="submit"
+                                value="LOG IN" 
+                                className={style.inputSubmit} />
+                        </form> : <CustomSpinner />
+                }
+                    <Link className={style.link} to="/register">CREATE A NEW ACCOUNT</Link>
             </article>
         </main>
     )
